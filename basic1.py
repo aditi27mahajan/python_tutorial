73d468c42cd927538f8c3246941f06fd94295f4d
aditi27mahajan <52156118+aditi27mahajan@users.noreply.github.com>
2019-06-24 15:05:42 +0530

    basic
    print("hello")
    print("i am aditi mahajan")
    
    # addition
    
    x=2
    y=3
    sum = x+y
    print(sum)
    
    
    #  subtracsion
    
    a=1
    b=2
    c=a+b
    print(c)
    
    
    # multiplication
    
    a=2
    b=2
    c=a*b
    print(c)
    
    # division
    
    a= 2
    b = 2
    n = a/b
    print(n)
    
    
    # remender
    
    a=2
    b=9
    m= a%b
    print(m)

---
+++ basic1.py
@@ -0,0 +1,40 @@
+print("hello")
+print("i am aditi mahajan")
+
+# addition
+
+x=2
+y=3
+sum = x+y
+print(sum)
+
+
+#  subtracsion
+
+a=1
+b=2
+c=a+b
+print(c)
+
+
+# multiplication
+
+a=2
+b=2
+c=a*b
+print(c)
+
+# division
+
+a= 2
+b = 2
+n = a/b
+print(n)
+
+
+# remender
+
+a=2
+b=9
+m= a%b
+print(m)
